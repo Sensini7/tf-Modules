1e348c56493dc04aa083f0e9478c45393cceeb0a
sensini7 <pelekengaih@gmail.com>
2024-10-28 05:52:57 +0100

    azurem linux rm updated

--- terraform/modules/app_service/main.tf
+++ terraform/modules/app_service/main.tf
@@ -1,11 +1,10 @@
-resource "azurerm_app_service" "app" {
+resource "azurerm_linux_web_app" "app" {
   name                = var.app_service_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  app_service_plan_id = var.app_service_plan_id
+  service_plan_id     = var.app_service_plan_id
 
   site_config {
-    linux_fx_version = var.app_runtime
     always_on        = true
     ftps_state       = "Disabled"
   }
--- terraform/modules/app_service/outputs.tf
+++ terraform/modules/app_service/outputs.tf
@@ -1,9 +1,7 @@
 output "default_site_hostname" {
-  description = "The default hostname of the App Service"
-  value       = azurerm_app_service.app.default_site_hostname
+  value = azurerm_linux_web_app.app.default_hostname
 }
 
 output "app_service_identity_principal_id" {
-  description = "Principal ID of the App Service's managed identity"
-  value       = azurerm_app_service.app.identity[0].principal_id
+  value = azurerm_linux_web_app.app.identity[0].principal_id
 }
