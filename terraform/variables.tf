57f384e56f4fdf5ec6a351f969586aac27a845ed
sensini7 <pelekengaih@gmail.com>
2024-10-28 03:11:10 +0100

    LOCATION CHANGED

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "resource_group_name" {
 variable "location" {
   description = "Azure region for resources"
   type        = string
-  default     = "East US"
+  default     = "Central US"
 }
 
 variable "app_service_plan_name" {
